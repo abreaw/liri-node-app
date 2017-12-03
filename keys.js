b63ceb4afa312dc0616d332bdd35e90691f107fe
Brea Torres <abreaw328@gmail.com>
2017-12-03 16:56:23 -0500

    initial file setup

---
+++ keys.js
@@ -0,0 +1,15 @@
+// show that the keys file has been loaded
+console.log('this is loaded');
+
+// add the real twitter key information here to access the test twitter account
+// https://apps.twitter.com/app/14539797/keys (BTTest117 account)
+var twitterKeys = {
+  consumer_key: '2SAvdi7YvJ3izNEA2LAJYvn0v',
+  consumer_secret: 'XrANYZQAqarj6PESKOAFkz6PwnIOCPVPiGtWBNvJ0rHmgcTQ3e',
+  access_token_key: '934260424446488577-oYm3EbunBBaa95A8QrIMBhXvsKvikPb',
+  access_token_secret: 'OYkk0BsWXY3FZ0AVdvXPX5BLMGVaXev3WShS4JvGi76QV',
+};
+
+// allow the key information to be imported / used in the main node.js app
+module.exports = twitterKeys;
+
